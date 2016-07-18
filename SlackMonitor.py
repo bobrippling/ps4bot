26df87ec3bab21993d353e684b4d195cfd91b4db
Rob Pilling <robpilling@gmail.com>
2016-07-18 20:21:49 +0100

    Pull out handle_slack_messages()

--- SlackMonitor.py
+++ SlackMonitor.py
@@ -22,43 +22,51 @@ class SlackMonitor():
             self.handlers[channel] = []
         self.handlers[channel].append(handler)
 
+    def handle_slack_messages(self):
+        handled = False
+
+        for slack_message in self.slackconnection.rtm_read():
+            if slack_message.get("type") is None:
+                continue
+
+            text = slack_message.get("text")
+            user = slack_message.get("user")
+            channel_id = slack_message.get("channel")
+            reply_to = slack_message.get("reply_to")
+            bot_id = slack_message.get("bot_id")
+
+            channel = channel_id # safe fallback
+            for i in self.slackconnection.server.channels:
+                if i.id == channel_id:
+                    channel = i
+                    break
+
+            if not text or not user or not channel:
+                continue
+
+            if channel.name not in self.handlers:
+                continue
+
+            handled = True
+
+            # anything from slack needs to be explicitly encoded as utf-8
+            text = ENCODE(text)
+            user = ENCODE(user)
+            bot_id = ENCODE(bot_id)
+
+            message = SlackMessage(text, user, channel, reply_to, bot_id)
+            for handler in self.handlers[channel.name]:
+                handler.set_current_channel(channel)
+                handler.handle_message(message)
+
+        return handled
+
     def run(self):
         idle_time = 0
         while True:
-            for slack_message in self.slackconnection.rtm_read():
-                if slack_message.get("type") is None:
-                    continue
-
-                text = slack_message.get("text")
-                user = slack_message.get("user")
-                channel_id = slack_message.get("channel")
-                reply_to = slack_message.get("reply_to")
-                bot_id = slack_message.get("bot_id")
-
-                channel = channel_id # safe fallback
-                for i in self.slackconnection.server.channels:
-                    if i.id == channel_id:
-                        channel = i
-                        break
-
-                if not text or not user or not channel:
-                    continue
-
-                if channel.name not in self.handlers:
-                    continue
-
-                # handling something, reset idle time
+            if self.handle_slack_messages():
+                # handled something, reset idle time
                 idle_time = 0
-
-                # anything from slack needs to be explicitly encoded as utf-8
-                text = ENCODE(text)
-                user = ENCODE(user)
-                bot_id = ENCODE(bot_id)
-
-                message = SlackMessage(text, user, channel, reply_to, bot_id)
-                for handler in self.handlers[channel.name]:
-                    handler.set_current_channel(channel)
-                    handler.handle_message(message)
 
             time.sleep(0.5)
             idle_time += 0.5
