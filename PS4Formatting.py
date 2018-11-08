72aa67a7e8fe362a50dd154ba574b003a00b95d7
Rob Pilling <robpilling@gmail.com>
2018-11-08 23:12:34 +0000

    Create when_from_str()

--- PS4Formatting.py
+++ PS4Formatting.py
@@ -1,5 +1,12 @@
+import datetime
+
+WHEN_FORMAT = "%H:%M"
+
 def when_str(when):
-    return when.strftime("%H:%M")
+    return when.strftime(WHEN_FORMAT)
+
+def when_from_str(s):
+    return datetime.datetime.strptime(s, WHEN_FORMAT)
 
 def format_user(user):
     return "<@{}>".format(user)
