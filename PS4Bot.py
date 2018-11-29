bobrippling/ps4bot

commit 56dabb767ed968bef458a68fe6956577cd15d561

Adds thankyou response

PS4Bot.py
@@ -372,6 +372,8 @@ class PS4Bot(Bot):
         self.send_message(reply)
 
     def handle_command(self, message, command, rest):
+        command = command.lower()
+        
         if len(command.strip()) == 0 and len(rest) == 0:
             self.send_dialect_reply(message)
         elif command == "nar":
@@ -384,6 +386,8 @@ class PS4Bot(Bot):
             self.join_or_bail(message, rest, bail = True)
         elif command == "scuttle":
             self.maybe_scuttle_game(message, rest)
+        elif command == "thanks" or command == "thankyou" or command == "thank you" or command == "ta" or command == "cheers":
+            self.send_message(("Nee bother {0}, you flippin' big lad!").format(format_user(message.user)))
         # attempt to parse a big game, if unsuccessful, show usage:
         elif not self.maybe_new_game(message.user, message.channel.name, command + " " + rest):
             self.send_message((

commit de9a76f06916fcdcbbe324ba5ccb6200bbf1ef65

Fix bug where first word of message was displayed in lower case
Introduced in 56dabb767ed968bef458a68fe6956577cd15d561

PS4Bot.py
@@ -622,14 +622,12 @@ class PS4Bot(Bot):
         self.update_stats_table(channel_name, stats, force_new = True, anchor_message = is_this_channel)
 
     def handle_command(self, message, command, rest):
-        command = command.lower()
-
         if len(command.strip()) == 0 and len(rest) == 0:
             self.send_dialect_reply(message)
             return
 
-        if command in PS4Bot_commands:
-            PS4Bot_commands[command][1](self, message, rest)
+        if command.lower() in PS4Bot_commands:
+            PS4Bot_commands[command.lower()][1](self, message, rest)
             return
 
         # attempt to parse a big game, if unsuccessful, show usage: