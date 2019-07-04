b9c90d6de4518711c42f149df5130b7fb22f027a
Rob Pilling <robpilling@gmail.com>
2019-07-04 18:20:09 +0100

    Add scoot as alias for scuttle

--- src/bots/ps4bot.py
+++ src/bots/ps4bot.py
@@ -38,6 +38,7 @@ PS4Bot_commands = {
     "nah": (None, lambda self, *args: self.maybe_cancel_game(*args)),
     "games": (Usage.games, lambda self, *args: self.show_games()),
     "scuttle": (Usage.scuttle, lambda self, *args: self.maybe_scuttle_game(*args)),
+    "scoot": (None, lambda self, *args: self.maybe_scuttle_game(*args)),
     "stats": (Usage.stats, lambda self, *args: self.handle_stats_request(*args)),
     "elo": (None, lambda self, *args: self.handle_stats_request(*args)),
     "credits": (None, lambda self, *args: self.send_credits(*args)),
